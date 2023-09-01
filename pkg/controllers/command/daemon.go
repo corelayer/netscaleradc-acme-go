0481b40fa99270dc9088f2f93e21c57b6c2bc42d
Jan Tytgat <jan.tytgat@corelayer.eu>
2023-09-01 17:35:53 +0200

    Updated daemon logic for certificate request, upload and configuraiton on NetScaler
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- pkg/controllers/command/daemon.go
+++ pkg/controllers/command/daemon.go
@@ -26,6 +26,7 @@ import (
 	"strconv"
 
 	"github.com/corelayer/netscaleradc-nitro-go/pkg/nitro"
+	nitroConfig "github.com/corelayer/netscaleradc-nitro-go/pkg/nitro/resource/config"
 	"github.com/corelayer/netscaleradc-nitro-go/pkg/nitro/resource/controllers"
 	"github.com/corelayer/netscaleradc-nitro-go/pkg/registry"
 	"github.com/go-acme/lego/certcrypto"
@@ -88,12 +89,12 @@ func (c Daemon) Execute() error {
 	return nil
 }
 
-func (c Daemon) updateNetScaler(config config.Certificate, certificates *certificate.Resource) error {
+func (c Daemon) updateNetScaler(certConfig config.Certificate, acmeCert *certificate.Resource) error {
 	var (
 		err error
 	)
 	var environments []registry.Environment
-	for _, b := range config.Bindpoints {
+	for _, b := range certConfig.Bindpoints {
 		var env registry.Environment
 		env, err = c.Config.GetEnvironment(b.Organization, b.Environment)
 		if err != nil {
@@ -110,23 +111,75 @@ func (c Daemon) updateNetScaler(config config.Certificate, certificates *certifi
 			return fmt.Errorf("could not get nitro client for environment %s with message %w", e.Name, err)
 		}
 		fc := controllers.NewSystemFileController(client)
-		certFilename := config.Name + "_" + c.Timestamp + ".cer"
-		pkeyFilename := config.Name + "_" + c.Timestamp + ".key"
-		location := "/nsconfig/ssl/CERTS/LENS/" + config.Name + "/"
+		certFilename := certConfig.Name + "_" + c.Timestamp + ".cer"
+		pkeyFilename := certConfig.Name + "_" + c.Timestamp + ".key"
+		location := "/nsconfig/ssl/CERTS/LENS/"
 		slog.Debug("uploading certificate public key to environment", "environment", e.Name)
-		_, err = fc.Add(certFilename, location, certificates.Certificate)
+		_, err = fc.Add(certFilename, location, acmeCert.Certificate)
 		if err != nil {
 			return fmt.Errorf("could not upload certificate public key to environment %s with message %w", e.Name, err)
 		}
 		slog.Debug("uploading certificate private key to environment", "environment", e.Name)
-
-		_, err = fc.Add(pkeyFilename, location, certificates.PrivateKey)
+		_, err = fc.Add(pkeyFilename, location, acmeCert.PrivateKey)
 		if err != nil {
 			return fmt.Errorf("could not upload certificate private key to environment %s with message %w", e.Name, err)
 		}
 
-	}
+		certKeyName := "LENS_" + certConfig.Name
+		certc := controllers.NewSslCertKeyController(client)
+		// Check if certificate exists
+		var certKey *nitro.Response[nitroConfig.SslCertKey]
+		if certKey, err = certc.Get(certKeyName, nil); err != nil {
+			if certKey.Message != "Certificate does not exist" {
+				slog.Error("could not verify if certificate exists in environment", "environment", e.Name, "error", err)
+				return fmt.Errorf("could not verify if certificate exists in environment %s with message %w", e.Name, err)
+			}
+		}
+		if certKey.Message == "Certificate does not exist" {
+			slog.Info("creating ssl certkey in environment", "environment", e.Name)
+			if _, err = certc.Add(certKeyName, location+certFilename, location+pkeyFilename); err != nil {
+				slog.Error("could not add certificate to environment", "environment", e.Name, "error", err)
+				return fmt.Errorf("could not add certificate to environment %s with message %w", e.Name, err)
+			}
+		} else {
+			slog.Info("updating ssl certkey in environment", "environment", e.Name)
+			if _, err = certc.Update(certKeyName, location+certFilename, location+pkeyFilename); err != nil {
+				slog.Error("could not update certificate exists in environment", "environment", e.Name, "error", err)
+				return fmt.Errorf("could not update certificate in environment %s with message %w", e.Name, err)
 
+			}
+		}
+
+		for _, b := range certConfig.Bindpoints {
+			var bindings *nitro.Response[nitroConfig.SslCertKey_SslVserver_Binding]
+			if bindings, err = certc.GetSslVserverBinding(certKeyName, nil); err != nil {
+				slog.Error("could not verify if certificate exists in environment", "environment", e.Name, "error", err)
+				return fmt.Errorf("could not verify if certificate exists in environment %s with message %w", e.Name, err)
+			}
+			if len(bindings.Data) == 0 {
+				for _, bindTo := range b.SslVservers {
+					if _, err = certc.Bind(bindTo.Name, certKeyName, bindTo.SniEnabled); err != nil {
+						slog.Error("could not bind certificate to vserver", "environment", e.Name, "certkey", certKeyName, "error", err)
+						// return fmt.Errorf("could not bind certificate %s to vserver in environment %s with message %w", certKeyName, e.Name, err)
+					}
+				}
+			} else {
+				for _, bindTo := range b.SslVservers {
+					for _, boundTo := range bindings.Data {
+						if bindTo.Name == boundTo.ServerName {
+							slog.Debug("certificate already bound to vserver", "certificate", certKeyName, "vserver", bindTo.Name)
+							break
+						}
+					}
+					slog.Debug("binding certificate to vserver", "certificate", certKeyName, "vserver", bindTo.Name)
+					if _, err = certc.Bind(bindTo.Name, certKeyName, bindTo.SniEnabled); err != nil {
+						slog.Error("could not bind certificate to vserver", "environment", e.Name, "certkey", certKeyName, "error", err)
+						// return fmt.Errorf("could not bind certificate %s to vserver in environment %s with message %w", certKeyName, e.Name, err)
+					}
+				}
+			}
+		}
+	}
 	return nil
 }
 
@@ -168,6 +221,7 @@ func (c Daemon) launchLego(config config.Certificate) (*certificate.Resource, er
 	// New users will need to register
 	var reg *registration.Resource
 	reg, err = client.Registration.Register(registration.RegisterOptions{TermsOfServiceAgreed: true})
+	// reg, err = client.Registration.QueryRegistration()
 	if err != nil {
 		slog.Error("could not register user", "error", err)
 		return nil, fmt.Errorf("could not register user %s for acme request with message %w", c.Config.User.Email, err)
