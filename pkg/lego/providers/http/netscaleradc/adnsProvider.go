d5ac0129a6bc34c59a199bd9437390f6562b61cc
Jan Tytgat <jan.tytgat@corelayer.eu>
2023-09-01 22:48:32 +0200

    Cleanup providers for go-acme/lego
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- pkg/lego/providers/http/netscaleradc/adnsProvider.go
+++ pkg/lego/providers/http/netscaleradc/adnsProvider.go
@@ -26,15 +26,16 @@ import (
 	"github.com/corelayer/netscaleradc-nitro-go/pkg/registry"
 )
 
+// ADnsProvider manages ACME requests for NetScaler ADC Authoritative DNS service
 type ADnsProvider struct {
 	nitroClient *nitro.Client
-	dnsTxt      *controllers.DnsTxtRecController
+	dnsTxtRec   *controllers.DnsTxtRecController
 
 	maxRetries int
 }
 
 // NewADnsProvider returns a HTTPProvider instance with a configured list of hosts
-func NewADnsProvider(environment registry.Environment, maxRetries int, timestamp string) (*ADnsProvider, error) {
+func NewADnsProvider(environment registry.Environment, maxRetries int) (*ADnsProvider, error) {
 	c := &ADnsProvider{
 		maxRetries: maxRetries,
 	}
@@ -43,63 +44,67 @@ func NewADnsProvider(environment registry.Environment, maxRetries int, timestamp
 }
 
 // Present the ACME challenge to the provider.
-// Parameter domain is the fqdn for which the challenge will be provided
+// domain is the fqdn for which the challenge will be provided
 // Parameter endpoint is the path to which ACME will look  for the challenge (/.well-known/acme-challenge/<token>)
 // Parameter keyAuth is the value which must be returned for a successful challenge
 func (p *ADnsProvider) Present(domain string, token string, keyAuth string) error {
 	var err error
-	slog.Info("prepare acme request", "domain", domain)
+	slog.Info("ns acme request", "type", "adns", "domain", domain)
 
-	slog.Debug("adding dns record", "domain", domain, "action")
-	if _, err = p.dnsTxt.Add(domain, []string{token}, 300); err != nil {
-		slog.Error("could not create dns record for acme challenge", "domain", domain)
-		return fmt.Errorf("could not create dns record for acme challenge for domain %s with error %w", domain, err)
+	// Add DNS record to ADNS zone on NetScaler ADC
+	slog.Debug("ns acme request: create dns record", "type", "adns", "domain", domain)
+	if _, err = p.dnsTxtRec.Add(domain, []string{token}, 300); err != nil {
+		slog.Error("ns acme request: could not create dns record", "type", "adns", "domain", domain, "error", err)
+		return fmt.Errorf("ns acme request: could not create dns record %s: %w", domain, err)
 	}
 
-	slog.Debug("prepare acme request completed", "domain", domain)
+	slog.Debug("ns acme request completed", "type", "adns", "domain", domain)
 	return nil
 }
 
 func (p *ADnsProvider) CleanUp(domain string, token string, keyAuth string) error {
 	var err error
-	slog.Info("cleanup acme request", "domain", domain)
+	slog.Info("ns acme cleanup", "type", "adns", "domain", domain)
 
-	slog.Debug("removing dns record", "domain", domain, "action")
-	var d *nitro.Response[config.DnsTxtRec]
-	if d, err = p.dnsTxt.Get(domain, []string{"recordid"}); err != nil {
-		slog.Error("could not get recordid for dns record for acme challenge", "domain", domain)
-		return fmt.Errorf("could not get recordid dns record for acme challenge for domain %s with error %w", domain, err)
+	slog.Debug("ns acme cleanup:remove dns record", "type", "adns", "domain", domain)
+	var res *nitro.Response[config.DnsTxtRec]
+	// Limit data transfer by limiting returned fields
+	if res, err = p.dnsTxtRec.Get(domain, []string{"recordid"}); err != nil {
+		slog.Error("ns acme cleanup: could not get recordid", "type", "adns", "domain", domain, "error", err)
+		return fmt.Errorf("ns acme cleanup: could not get recordid %s: %w", domain, err)
 
 	}
 
-	for _, rec := range d.Data {
+	for _, rec := range res.Data {
+		// Loop over array of returned records
 		for _, data := range rec.Data {
+			// Only remove record if keyAuth matches the current acme request
 			if data != keyAuth {
 				continue
 			}
-			// Can multiple records exist?
-			if _, err = p.dnsTxt.Delete(domain, d.Data[0].RecordId); err != nil {
-				slog.Error("could not delete dns record for acme challenge", "domain", domain)
-				return fmt.Errorf("could not delete dns record for acme challenge for domain %s with error %w", domain, err)
+
+			if _, err = p.dnsTxtRec.Delete(domain, rec.RecordId); err != nil {
+				slog.Error("ns acme cleanup: could not remove dns record", "type", "adns", "domain", domain, "error", err)
+				return fmt.Errorf("ns acme cleanup: could not remove dns record %s: %w", domain, err)
 			}
 			break
 		}
 	}
 
-	slog.Debug("cleanup acme request completed", "domain", domain)
+	slog.Debug("ns acme cleanup completed", "type", "adns", "domain", domain)
 	return nil
 }
 
 func (p *ADnsProvider) initialize(e registry.Environment) error {
-	slog.Debug("initialize nitro client for primary node for environment", "environment", e.Name)
+	slog.Debug("ns acme adns provider initialization", "environment", e.Name)
 	client, err := e.GetPrimaryNitroClient()
 	if err != nil {
-		return fmt.Errorf("failed to initialize ADnsProvider with error %w", err)
+		slog.Error("ns acme adns provider initialization failed", "error", err)
+		return fmt.Errorf("ns acme adns provider initialization failed: %w", err)
 	}
 
-	slog.Debug("initialize nitro controllers for responder functionality")
 	p.nitroClient = client
-	p.dnsTxt = controllers.NewDnsTxtRecController(client)
-
+	p.dnsTxtRec = controllers.NewDnsTxtRecController(client)
+	slog.Debug("ns acme adns provider initialization completed")
 	return nil
 }
--- pkg/lego/providers/http/netscaleradc/globalHttpProvider.go
+++ pkg/lego/providers/http/netscaleradc/globalHttpProvider.go
@@ -27,6 +27,7 @@ import (
 	"github.com/corelayer/netscaleradc-nitro-go/pkg/registry"
 )
 
+// GlobalHttpProvider manages ACME requests for NetScaler ADC using globally bound responder policies
 type GlobalHttpProvider struct {
 	nitroClient    *nitro.Client
 	rsaController  *controllers.ResponderActionController
@@ -51,183 +52,220 @@ func NewGlobalHttpProvider(environment registry.Environment, maxRetries int, tim
 	return c, c.initialize(environment)
 }
 
-// Present the ACME challenge to the provider.
-// Parameter domain is the fqdn for which the challenge will be provided
-// Parameter endpoint is the path to which ACME will look  for the challenge (/.well-known/acme-challenge/<token>)
-// Parameter keyAuth is the value which must be returned for a successful challenge
+// Present the ACME challenge to the provider before validation
+//
+//	domain is the fqdn for which the challenge will be provided
+//	token is the path to which ACME will look  for the challenge (/.well-known/acme-challenge/<token>)
+//	keyAuth is the value which must be returned for a successful challenge
 func (p *GlobalHttpProvider) Present(domain string, token string, keyAuth string) error {
 	var err error
-	slog.Info("prepare acme request", "domain", domain)
+	slog.Info("ns acme request", "type", "global http", "domain", domain)
 
-	slog.Debug("adding responder action", "domain", domain, "action", p.getResponderActionName(domain))
+	rsaActionName := p.getResponderActionName(domain)
+	rspPolicyName := p.getResponderPolicyName(domain)
 	rsaAction := "\"HTTP/1.1 200 OK\\r\\n\\r\\n" + keyAuth + "\""
-	if _, err = p.rsaController.Add(p.getResponderActionName(domain), "respondwith", rsaAction); err != nil {
-		slog.Error("could not create responder action for acme challenge", "domain", domain, "action", p.getResponderActionName(domain))
-		return fmt.Errorf("could not create responder action %s for acme challenge for domain %s with error %w", p.getResponderActionName(domain), domain, err)
-	}
-
-	slog.Debug("adding responder policy", "domain", domain, "policy", p.getResponderPolicyName(domain))
 	rspRule := "HTTP.REQ.HOSTNAME.EQ(\"" + domain + "\") && HTTP.REQ.URL.EQ(\"/.well-known/acme-challenge/" + token + "\")"
-	if _, err = p.rspController.Add(p.getResponderPolicyName(domain), rspRule, p.getResponderActionName(domain), ""); err != nil {
-		slog.Error("could not create responder policy for acme challenge", "domain", domain, "policy", p.getResponderPolicyName(domain))
-		return fmt.Errorf("could not create responder policy %s for acme challenge for domain %s with error %w", p.getResponderPolicyName(domain), domain, err)
+
+	// Create responder action
+	slog.Debug("ns acme request: create responder action", "type", "global http", "domain", domain, "resource", rsaActionName)
+	if _, err = p.rsaController.Add(rsaActionName, "respondwith", rsaAction); err != nil {
+		slog.Error("ns acme request: could not create responder action", "type", "global http", "domain", domain, "resource", rsaActionName)
+		return fmt.Errorf("ns acme request: could not create responder action %s for %s: %w", rsaActionName, domain, err)
 	}
 
+	// Create responder policy
+	slog.Debug("ns acme request: create responder policy", "type", "global http", "domain", domain, "resource", rspPolicyName)
+	if _, err = p.rspController.Add(rspPolicyName, rspRule, rsaActionName, ""); err != nil {
+		slog.Error("ns acme request: could not create responder policy", "type", "global http", "domain", domain, "resource", rspPolicyName)
+		return fmt.Errorf("ns acme request: could not create responder policy %s for %s: %w", rspPolicyName, domain, err)
+	}
+
+	// Bind responder policy to global REQ_OVERRIDE
+	// We need REQ_OVERRIDE, otherwise responder policies bound to a csvserver/lbvserver get a higher priority
 	if err = p.bindResponderPolicy(domain); err != nil {
-		slog.Error("could not bind global responder policy for acme challenge", "domain", domain)
+		slog.Error("ns acme request: could not bind global responder policy", "type", "global http", "domain", domain, "resource", rspPolicyName)
+		return fmt.Errorf("ns acme request: could not bind global responder policy %s for %s: %w", rspPolicyName, domain, err)
 	}
 
-	slog.Debug("prepare acme request completed", "domain", domain)
+	slog.Debug("ns acme request completed", "type", "global http", "domain", domain)
 	return nil
 }
 
+// CleanUp the ACME challenge on the provider after validation
+//
+//	domain is the fqdn for which the challenge will be provided
+//	token is the path to which ACME will look  for the challenge (/.well-known/acme-challenge/<token>)
+//	keyAuth is the value which must be returned for a successful challenge
 func (p *GlobalHttpProvider) CleanUp(domain string, token string, keyAuth string) error {
 	var err error
-	slog.Info("cleanup acme request", "domain", domain)
+	slog.Info("ns acme cleanup", "type", "global http", "domain", domain)
 
-	slog.Debug("cleaning up global responder policy binding", "domain", domain, "policy", p.getResponderPolicyName(domain))
-	if _, err = p.rspbController.Delete(p.getResponderPolicyName(domain), p.rspbBindtype); err != nil {
-		slog.Error("could not unbind global responder policy for acme challenge", "domain", domain, "policy", p.getResponderPolicyName(domain))
-		return fmt.Errorf("could not unbind global responder policy %s for acme challenge for domain %s with message %w", p.getResponderPolicyName(domain), domain, err)
+	rspPolicyName := p.getResponderPolicyName(domain)
+	rsaActionName := p.getResponderActionName(domain)
+
+	// Unbind responder policy from global REQ_OVERRIDE
+	slog.Debug("ns acme cleanup: unbind global responder policy", "type", "global http", "domain", domain, "resource", rspPolicyName)
+	if _, err = p.rspbController.Delete(rspPolicyName, p.rspbBindtype); err != nil {
+		slog.Error("ns acme cleanup: could not unbind global responder policy", "type", "global http", "domain", domain, "resource", rspPolicyName)
+		return fmt.Errorf("ns acme cleanup: could not unbind global responder policy %s for %s: %w", rspPolicyName, domain, err)
 	}
 
-	slog.Debug("cleaning up responder policy", "domain", domain, "policy", p.getResponderPolicyName(domain))
-	if _, err = p.rspController.Delete(p.getResponderPolicyName(domain)); err != nil {
-		slog.Error("could not remove responder policy for acme challenge", "domain", domain, "policy", p.getResponderPolicyName(domain))
-		return fmt.Errorf("could not remove responder policy %s for acme challenge for domain %s with error %w", p.getResponderPolicyName(domain), domain, err)
+	slog.Debug("ns acme cleanup: remove responder policy", "type", "global http", "domain", domain, "resource", rspPolicyName)
+	if _, err = p.rspController.Delete(rspPolicyName); err != nil {
+		slog.Error("ns acme cleanup: could not remove responder policy", "type", "global http", "domain", domain, "resource", rspPolicyName)
+		return fmt.Errorf("ns acme cleanup: could not remove responder policy %s for %s: %w", rspPolicyName, domain, err)
 	}
 
-	slog.Debug("cleaning up responder action", "domain", domain, "action", p.getResponderActionName(domain))
-	if _, err = p.rsaController.Delete(p.getResponderActionName(domain)); err != nil {
-		slog.Error("could not remove responder action for acme challenge", "domain", domain, "action", p.getResponderActionName(domain))
-		return fmt.Errorf("could not remove responder action %s for acme challenge for domain %s with error %w", p.getResponderActionName(domain), domain, err)
+	slog.Debug("ns acme cleanup: remove responder action", "type", "global http", "domain", domain, "resource", rsaActionName)
+	if _, err = p.rsaController.Delete(rsaActionName); err != nil {
+		slog.Error("ns acme cleanup: could not remove responder action", "type", "global http", "domain", domain, "resource", rsaActionName)
+		return fmt.Errorf("ns acme cleanup: could not remove responder action %s for %s: %w", rsaActionName, domain, err)
 	}
 
-	slog.Debug("acme request cleanup completed", "domain", domain)
+	slog.Debug("ns acme cleanup completed", "type", "global http", "domain", domain)
 	return nil
 }
 
+// bindResponderPolicy will bind the responder policy globally on NetScaler
 func (p *GlobalHttpProvider) bindResponderPolicy(domain string) error {
 	var (
 		successfullyBoundPolicy = false
 		retries                 = 0
 		err                     error
 		priority                string
+		rspPolicyName           = p.getResponderPolicyName(domain)
 	)
 
 	for !successfullyBoundPolicy {
-		slog.Debug("search for valid binding priority", "domain", domain)
+		slog.Debug("ns acme request: search for valid binding priority", "type", "global http", "domain", domain, "resource", rspPolicyName)
 
 		retries += 1
 		priority, err = p.getPriority()
 		if err != nil {
-			return fmt.Errorf("could not get valid global policy binding priority for acme challenge for domain %s with message %w", domain, err)
+			slog.Error("ns acme request: could not find valid policy binding priority", "type", "global http", "domain", domain, "error", err)
+			return fmt.Errorf("ns acme request: could not find valid policy binding priority for %s: %w", domain, err)
 		}
 
-		slog.Debug("binding global responder policy", "policy", p.getResponderPolicyName(domain), "priority", priority)
-		if _, err = p.rspbController.Add(p.getResponderPolicyName(domain), p.rspbBindtype, priority, ""); err != nil {
-			if retries == p.maxRetries {
-				return fmt.Errorf("could not bind global responder policy for acme challenge for domain %s with message %w", domain, err)
+		if _, err = p.rspbController.Add(rspPolicyName, p.rspbBindtype, priority, "END"); err != nil {
+			if retries >= p.maxRetries {
+				slog.Error("ns acme request: exceeded max retries to bind global responder policy", "type", "global http", "domain", domain, "resource", rspPolicyName)
+				return fmt.Errorf("ns acme request: exceeded max retries to bind global responder policy %s for %s: %w", rspPolicyName, domain, err)
 			}
+			// If the attempt to bind the policy at the current priority fails, continue to the next iteration to increase the priority
 			continue
 		}
+		// The binding completed successfully, exit the loop
 		successfullyBoundPolicy = true
 	}
 	return nil
 }
 
+// getPolicyBindingPriorities will get all global responder binding priorities currently in use on NetScaler
+func (p *GlobalHttpProvider) getPolicyBindingPriorities() ([]string, error) {
+	var (
+		err      error
+		output   []string
+		bindings *nitro.Response[config.ResponderGlobalResponderPolicyBinding]
+	)
+	slog.Debug("ns acme request: retrieve existing priorities", "type", "global http")
+
+	// Create custom Nitro Request
+	// Limit data transfer by limiting returned fields
+	nitroRequest := &nitro.Request[config.ResponderGlobalResponderPolicyBinding]{
+		Arguments: map[string]string{
+			"type": p.rspbBindtype,
+		},
+		Attributes: []string{"priority"},
+	}
+
+	// Execute Nitro Request
+	bindings, err = nitro.ExecuteNitroRequest[config.ResponderGlobalResponderPolicyBinding](p.nitroClient, nitroRequest)
+	if err != nil {
+		slog.Error("ns acme request: could not retrieve existing priorities", "type", "global http")
+		return nil, fmt.Errorf("ns acme request: could not retrieve existing priorities: %w", err)
+	}
+
+	// If no priorities are found, the nitro request will return an empty slice, so we can return immediately
+	if len(bindings.Data) == 0 {
+		slog.Debug("ns acme request: no global responder policy bindings found", "type", "global http", "count", len(bindings.Data))
+		return output, nil
+	}
+
+	// If there are policies bound, add existing priorities to the list
+	for _, binding := range bindings.Data {
+		slog.Debug("ns acme request: add existing priority to list", "type", "global http", "priority", binding.Priority)
+		output = append(output, binding.Priority)
+	}
+	return output, nil
+}
+
+// getPriority finds an available priority for binding the responder policy
+func (p *GlobalHttpProvider) getPriority() (string, error) {
+	var (
+		err                error
+		priority           float64 = 33500
+		usedPriorities     []string
+		validPriorityFound bool = false
+	)
+	slog.Debug("ns acme request: find valid priority for binding", "type", "global http")
+
+	usedPriorities, err = p.getPolicyBindingPriorities()
+	if err != nil {
+		return "", err
+	}
+
+	// If there are no existing priorities, use the deault value + 1
+	if len(usedPriorities) == 0 {
+		priority = priority + 1
+		slog.Debug("ns acme request: using default priority", "type", "global http", "priority", priority)
+		return fmt.Sprintf("%g", priority), nil
+	}
+
+	// Existing priorities are found, find available priority
+	for !validPriorityFound {
+		priority = priority + 1
+		validPriorityFound = !p.priorityExists(priority, usedPriorities)
+	}
+	slog.Debug("ns acme request: found available priority", "type", "global http", "priority", priority)
+	return fmt.Sprintf("%g", priority), nil
+}
+
+// priorityExists checks if a priority is present in a slice or priorities
+//
+//	priority is the desired priority
+//	usedPriorities is the current list of priorities in use
 func (p *GlobalHttpProvider) priorityExists(priority float64, usedPriorities []string) bool {
 	if len(usedPriorities) == 0 {
-		slog.Debug("priorityExists", "exists", false, "priority", priority)
 		return false
 	}
 
 	for _, usedPriority := range usedPriorities {
 		// Convert priority to string --> exponent as needed, necessary digits only
 		if fmt.Sprintf("%g", priority) == usedPriority {
-			slog.Debug("priorityExists", "exists", true, "priority", priority)
+			slog.Debug("ns acme request: priority is in use", "type", "global http", "priority", priority)
 			return true
 		}
 	}
-	slog.Debug("priorityExists processed all usedPriorities", "exists", false, "priority", priority)
+	slog.Debug("ns acme request: priority is not in use", "type", "global http", "priority", priority)
 	return false
 }
 
-func (p *GlobalHttpProvider) generateAcmeUrl(token string) string {
-	return "/.well-known/acme-challenge/" + token
-}
-
-func (p *GlobalHttpProvider) getPriority() (string, error) {
-	var (
-		err                error
-		priority           float64
-		usedPriorities     []string
-		validPriorityFound bool
-	)
-	priority = 33500
-	validPriorityFound = false
-
-	usedPriorities, err = p.getPolicyBindingPriorities()
-	if err != nil {
-		return "", fmt.Errorf("could not determine valid binding priority with error %w", err)
-	}
-
-	if len(usedPriorities) == 0 {
-		priority = priority + 1
-		slog.Debug("no global responder policy bindings found, using default priority", "priority", priority)
-		return fmt.Sprintf("%g", priority), nil
-	}
-
-	for !validPriorityFound {
-		priority = priority + 1
-		validPriorityFound = !p.priorityExists(priority, usedPriorities)
-	}
-	return fmt.Sprintf("%g", priority), nil
-}
-
-func (p *GlobalHttpProvider) getPolicyBindingPriorities() ([]string, error) {
-	var output []string
-	slog.Debug("get used priorities for global responder policy bindings")
-
-	bindingsNitroRequest := &nitro.Request[config.ResponderGlobalResponderPolicyBinding]{
-		Arguments: map[string]string{
-			"type": "REQ_OVERRIDE",
-		},
-		Attributes: []string{"priority"},
-	}
-
-	var err error
-	var bindings *nitro.Response[config.ResponderGlobalResponderPolicyBinding]
-	bindings, err = nitro.ExecuteNitroRequest[config.ResponderGlobalResponderPolicyBinding](p.nitroClient, bindingsNitroRequest)
-	if err != nil {
-		return nil, fmt.Errorf("could not get list of globally bound responder policies with error %w", err)
-	}
-
-	if len(bindings.Data) == 0 {
-		slog.Debug("no global responder policy bindings found", "count", len(bindings.Data))
-		return output, nil
-	}
-
-	for _, binding := range bindings.Data {
-		slog.Debug("adding priority to list", "priority", binding.Priority)
-		output = append(output, binding.Priority)
-	}
-	return output, nil
-}
-
+// getResponderActionName generates the name for the responder action
 func (p *GlobalHttpProvider) getResponderActionName(domain string) string {
 	return p.rsaPrefix + domain + "_" + p.timestamp
 }
 
+// getResponderPolicyName generates the name for the responder policy
 func (p *GlobalHttpProvider) getResponderPolicyName(domain string) string {
 	return p.rspPrefix + domain + "_" + p.timestamp
 }
 
 func (p *GlobalHttpProvider) initialize(e registry.Environment) error {
-	slog.Debug("initialize nitro client for primary node for environment", "environment", e.Name)
+	slog.Debug("ns acme global http provider initialization", "environment", e.Name)
 	client, err := e.GetPrimaryNitroClient()
 	if err != nil {
-		return fmt.Errorf("failed to initialize GlobalHttpProvider with error %w", err)
+		slog.Error("ns acme global http provider initialization failed", "error", err)
+		return fmt.Errorf("ns acme global http provider initialization failed: %w", err)
 	}
 
 	slog.Debug("initialize nitro controllers for responder functionality")
@@ -243,5 +281,6 @@ func (p *GlobalHttpProvider) initialize(e registry.Environment) error {
 	p.rsaPrefix = "RSA_LENS_"
 	p.rspPrefix = "RSP_LENS_"
 
+	slog.Debug("ns acme global http provider initialization completed", "environment", e.Name)
 	return nil
 }
